eabcbc0a5d1ef06c042a4bcc97021ff737507e80
anilgyawali0623 <anilgyawali8937@gmail.com>
2024-11-09 17:17:50 +0545

    1

--- contentScript.js
+++ contentScript.js
@@ -1,7 +1,32 @@
-(() => {
-    let youtubeLeftControls, youtubePlayer;
+() => {
+  let currentVideo = "";
+  let youtubeLeftControls, youtubePlayer;
+  chrome.runtime.onMessage.addListener((obj, sender, response) => {
+    const { type, value, videoId } = obj;
 
+    if (type === "NEW") {
+      currentVideo = videoId;
+      newVideoLoaded();
+    }
+  });
 
+  const newVideoLoaded = async () => {
+    const bookmarkBtnExists =
+      document.getElementsByClassName("bookmark-btn")[0];
 
+    if (!bookmarkBtnExists) {
+      const bookmarkBtn = document.createElement("img");
+      bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
+      bookmarkBtn.className = "ytp-button" + "bookmark-btn";
+      bookmarkBtn.title = "click to book mark";
 
-})
+      youtubeLeftControls =
+        document.getElementsByClassName("ytp-left-controls")[0];
+
+      youtubePlayer = document.getElementsByClassName("video-stream")[0];
+      youtubeLeftControls.appendChild(bookmarkBtn);
+    }
+  };
+
+  newVideoLoaded();
+};
